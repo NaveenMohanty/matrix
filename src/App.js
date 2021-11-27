4bf2af9a29d3ea7f6e7b211441f1ccf26592779e
Naveen Mohanty <naveen.mohanty@namasys.co>
2021-11-27 16:49:31 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
 
+import "./App.css";
+import React, { useState } from "react";
+let arr = [
+  [1, 2, 3, 4],
+  [5, 6, 7, 8],
+  [9, 10, 11, 12],
+  [13, 14, 15, 16],
+];
 function App() {
+  const [clicked, setClicked] = useState([]);
+  const onClickhandle = (idx) => {
+    let newArr = clicked;
+    newArr.push(idx);
+    if (newArr.length > 2) {
+      newArr.shift();
+    }
+    setClicked([...newArr]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ margin: "auto" }}>
+      {arr.map((m, i) => {
+        return (
+          <div
+            style={{
+              display: "flex",
+            }}
+          >
+            {m.map((a, j) => (
+              <button
+                style={{
+                  height: "50px",
+                  width: "50px",
+                  backgroundColor: clicked.includes(a) ? "red" : "purple",
+                }}
+                onClick={() => onClickhandle(a)}
+              >
+                {a}
+              </button>
+            ))}
+          </div>
+        );
+      })}
     </div>
   );
 }
